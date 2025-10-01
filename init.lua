f876e3fd56012a97a843720a8da9946fff76cf83
Elie KADOCHE <eliekadoche78@gmail.com>
2025-10-01 17:50:56 +0200

    Update nvim-treesiter

--- init.lua
+++ init.lua
@@ -180,61 +180,59 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 -- Install lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-    if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out,                            "WarningMsg" },
-            { "\nPress any key to exit..." },
-        }, true, {})
-        vim.fn.getchar()
-        os.exit(1)
-    end
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Plugins
 require("lazy").setup({
+    spec = {
     -- nvim-treesitter
     -- ------------------------------------------
     -- ------------------------------------------
     {
         "nvim-treesitter/nvim-treesitter",
-        branch = 'master',
+        branch = "main",
         lazy = false,
         build = ":TSUpdate",
         config = function()
-            local configs = require("nvim-treesitter.configs")
-            configs.setup({
-                ensure_installed = {
-                    "bash",
-                    "bibtex",
-                    "c",
-                    "cmake",
-                    "cpp",
-                    "csv",
-                    "diff",
-                    "git_config",
-                    "git_rebase",
-                    "gitattributes",
-                    "gitcommit",
-                    "gitignore",
-                    "lua",
-                    "make",
-                    "markdown",
-                    "markdown_inline",
-                    "python",
-                    "ssh_config",
-                    "vim",
-                    "vimdoc",
-                    "yaml",
-                },
-                sync_install = false,
-                highlight = { enable = true },
-                indent = { enable = true },
-                auto_install = true,
-            })
+            require("nvim-treesitter").setup {
+                -- Directory to install parsers and queries to
+                install_dir = vim.fn.stdpath("data") .. "/site",
+            }
+            require("nvim-treesitter").install {
+                "bash",
+                "bibtex",
+                "c",
+                "cmake",
+                "cpp",
+                "csv",
+                "diff",
+                "git_config",
+                "git_rebase",
+                "gitattributes",
+                "gitcommit",
+                "gitignore",
+                "lua",
+                "make",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "ssh_config",
+                "vim",
+                "vimdoc",
+                "yaml",
+            }
         end,
     },
 
@@ -701,6 +699,11 @@ plt.show()]],
             })
         end,
     },
+    },
+    -- Disable luarocks
+        rocks = { enabled = false },
+      -- Automatically check for plugin updates
+      checker = { enabled = true },
 })
 
 -- Use vim colorscheme
