a5a87fc74463248bb5389124109dfd4637ac5d9a
Elie KADOCHE <eliekadoche78@gmail.com>
2025-10-11 19:52:29 +0200

    Add ruff

--- install_termux.sh
+++ install_termux.sh
@@ -63,6 +63,7 @@ pkg install -y python-numpy
 pkg install -y python-torch
 pkg install -y rsync
 pkg install -y ruby
+pkg install -y ruff
 pkg install -y rust
 pkg install -y screen
 pkg install -y stylua
