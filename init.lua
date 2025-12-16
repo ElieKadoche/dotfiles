6ac4eff1e08bbd43fe64cc24609d7160966fc983
Elie KADOCHE <eliekadoche78@gmail.com>
2025-12-16 10:24:50 +0100

    Add CSV plugin

--- init.lua
+++ init.lua
@@ -400,6 +400,28 @@ require("lazy").setup({
             },
         },
 
+        -- csvview.nvim
+        -- ------------------------------------------
+        -- ------------------------------------------
+        {
+            "hat0uma/csvview.nvim",
+            ---@module "csvview"
+            ---@type CsvView.Options
+            opts = {
+                parser = { comments = { "#", "//" } },
+                keymaps = {
+                    -- Text objects for selecting fields
+                    textobject_field_inner = { "if", mode = { "o", "x" } },
+                    textobject_field_outer = { "af", mode = { "o", "x" } },
+                    jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
+                    jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
+                    jump_next_row = { "<Enter>", mode = { "n", "v" } },
+                    jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
+                },
+            },
+            cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
+        },
+
         -- nvim-lspconfig
         -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
         -- ------------------------------------------
